0343bebffdec4c62adbfcc09d734a81362917eed
Phillipp Schoppmann <schoppmann@informatik.hu-berlin.de>
2017-07-10 17:44:14 +0200

    Use empty Deleter when returning shared_ptr to existing objects

--- include/CryptoInfra/PlainText.hpp
+++ include/CryptoInfra/PlainText.hpp
@@ -1,28 +1,28 @@
 /**
 * %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-* 
+*
 * Copyright (c) 2016 LIBSCAPI (http://crypto.biu.ac.il/SCAPI)
 * This file is part of the SCAPI project.
 * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
-* 
+*
 * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
-* to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+* to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
 * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-* 
+*
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-* 
+*
 * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
 * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
 * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-* 
+*
 * We request that any publication and/or code referring to and/or based on SCAPI contain an appropriate citation to SCAPI, including a reference to
 * http://crypto.biu.ac.il/SCAPI.
-* 
+*
 * Libscapi uses several open source libraries. Please see these projects for any further licensing issues.
 * For more information , See https://github.com/cryptobiu/libscapi/blob/master/LICENSE.MD
 *
 * %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-* 
+*
 */
 
 
@@ -43,7 +43,7 @@ class PlaintextSendableData : public NetworkSerialized {};
 class Plaintext {
 public:
 	/**
-	* This function is used when a Plaintex needs to be sent via 
+	* This function is used when a Plaintex needs to be sent via
 	* a Channel or any other means of sending data (including serialization).
 	* It retrieves all the data needed to reconstruct this Plaintext at a later time
 	* and/or in a different environment.
@@ -74,12 +74,11 @@ public:
 	};
 
 	shared_ptr<PlaintextSendableData> generateSendableData() override {
-		// since BigIntegerPlainText is both a Plaintext and a PlaintextSendableData, 
-		// on the one hand it has to implement the generateSendableData() function, 
+		// since BigIntegerPlainText is both a Plaintext and a PlaintextSendableData,
+		// on the one hand it has to implement the generateSendableData() function,
 		// but on the other hand it is in itself an PlaintextSendableData, so we do not really
 		// generate sendable data, but just return this object.
-		shared_ptr<PlaintextSendableData> res(this);
-		return res;
+		return shared_ptr<PlaintextSendableData>(this, [](void*){});
 	}
 
 	string toString() override { return (string)x; };
@@ -111,12 +110,11 @@ public:
 		return true;
 	};
 	shared_ptr<PlaintextSendableData> generateSendableData() override {
-		// since ByteArrayPlainText is both a Plaintext and a PlaintextSendableData, 
-		// on the one hand it has to implement the generateSendableData() function, 
+		// since ByteArrayPlainText is both a Plaintext and a PlaintextSendableData,
+		// on the one hand it has to implement the generateSendableData() function,
 		// but on the other hand it is in itself an PlaintextSendableData, so we do not really
 		// generate sendable data, but just return this object.
-		shared_ptr<PlaintextSendableData> res(this);
-		return res;
+		return shared_ptr<PlaintextSendableData>(this, [](void*){});
 	};
 
 	string toString() override {
@@ -124,7 +122,7 @@ public:
 		return string(reinterpret_cast<char const*>(uc), text.size());
 	};
 
-	void initFromString(const string & raw) override { 
+	void initFromString(const string & raw) override {
 		text.assign(raw.begin(), raw.end()); }
 
 };
@@ -147,7 +145,7 @@ public:
 	};
 
 	shared_ptr<PlaintextSendableData> generateSendableData() override {
-		
+
 		return make_shared<GroupElementPlaintextSendableData>(element->generateSendableData());
 	}
 
@@ -225,7 +223,7 @@ public:
 		//Since BigIntegerCiphertext is both an AsymmetricCiphertext and a AsymmetricCiphertextSendableData, on the one hand it has to implement
 		//the generateSendableData() function, but on the other hand it is in itself an AsymmetricCiphertextSendableData, so we do not really
 		//generate sendable data, but just return this object.
-		return shared_ptr<AsymmetricCiphertextSendableData>(this);
+		return shared_ptr<AsymmetricCiphertextSendableData>(this, [](void*){});
 	}
 
 	bool operator==(const AsymmetricCiphertext &other) const {
